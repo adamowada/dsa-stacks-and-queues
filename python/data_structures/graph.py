695d987c6a90f01f828c555b7202dcd964e26624
Adam Owada <adamowada@gmail.com>
2024-02-22 23:06:50 -0800

    graph complete

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -4,43 +4,52 @@ class Vertex:
     def __init__(self, value):
         self.value = value
 
+    def __repr__(self):
+        return f"{self.value}"
+
 
 class Edge:
     """An Edge connecting two vertices in a graph, with an optional weight."""
 
-    def __init__(self, vertex, weight=1):
+    def __init__(self, vertex, weight=None):
         self.vertex = vertex
         self.weight = weight
+
+    def __repr__(self):
+        return f"Edge to {self.vertex}{f' with weight {self.weight}' if self.weight else '' }"
 
 
 class Graph:
     """A graph implemented using an adjacency list."""
 
     def __init__(self):
-        self._adjacency_list = {}
+        self.adjacency_list = {}
+
+    def __str__(self):
+        return f"Graph {self.adjacency_list}"
 
     def add_node(self, value):
         """Add a new vertex to the graph."""
         vertex = Vertex(value)
-        self._adjacency_list[vertex] = []
+        self.adjacency_list[vertex] = []
         return vertex
 
     def add_edge(self, start_vertex, end_vertex, weight=1):
         """Add a directed edge from start_vertex to end_vertex."""
-        if start_vertex not in self._adjacency_list or end_vertex not in self._adjacency_list:
+        if start_vertex not in self.adjacency_list or end_vertex not in self.adjacency_list:
             raise KeyError("Both vertices must exist in the graph.")
         edge = Edge(end_vertex, weight)
-        self._adjacency_list[start_vertex].append(edge)
+        self.adjacency_list[start_vertex].append(edge)
 
     def get_nodes(self):
         """Return a collection of all vertices in the graph."""
-        return list(self._adjacency_list.keys())
+        return list(self.adjacency_list.keys())
 
     def get_neighbors(self, vertex):
         """Return a collection of edges connected to the given vertex."""
-        return self._adjacency_list.get(vertex, [])
+        return self.adjacency_list[vertex]
 
     def size(self):
         """Return the number of vertices in the graph."""
-        return len(self._adjacency_list)
+        return len(self.adjacency_list)
 
