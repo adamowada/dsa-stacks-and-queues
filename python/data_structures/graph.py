07ca74584fa799c4f708bf2c115a7c8c63628dac
Adam Owada <adamowada@gmail.com>
2024-02-22 22:34:25 -0800

    graph code

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -1,16 +1,46 @@
+class Vertex:
+    """A Vertex in a graph."""
+
+    def __init__(self, value):
+        self.value = value
+
+
+class Edge:
+    """An Edge connecting two vertices in a graph, with an optional weight."""
+
+    def __init__(self, vertex, weight=1):
+        self.vertex = vertex
+        self.weight = weight
+
+
 class Graph:
-    """
-    Put docstring here
-    """
+    """A graph implemented using an adjacency list."""
 
     def __init__(self):
-        # initialization here
-        pass
+        self._adjacency_list = {}
 
-    def some_method(self):
-        # method body here
-        pass
+    def add_node(self, value):
+        """Add a new vertex to the graph."""
+        vertex = Vertex(value)
+        self._adjacency_list[vertex] = []
+        return vertex
 
+    def add_edge(self, start_vertex, end_vertex, weight=1):
+        """Add a directed edge from start_vertex to end_vertex."""
+        if start_vertex not in self._adjacency_list or end_vertex not in self._adjacency_list:
+            raise KeyError("Both vertices must exist in the graph.")
+        edge = Edge(end_vertex, weight)
+        self._adjacency_list[start_vertex].append(edge)
 
-class Vertex:
-    pass
+    def get_nodes(self):
+        """Return a collection of all vertices in the graph."""
+        return list(self._adjacency_list.keys())
+
+    def get_neighbors(self, vertex):
+        """Return a collection of edges connected to the given vertex."""
+        return self._adjacency_list.get(vertex, [])
+
+    def size(self):
+        """Return the number of vertices in the graph."""
+        return len(self._adjacency_list)
+
